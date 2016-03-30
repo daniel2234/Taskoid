59f2db211feaac800adddd7d621c7b84d19f8b7f
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2016-03-30 10:18:34 -0400

    added an arrayAdapter for dynamic arrays

--- app/src/main/java/programs/taskoid/TaskListActivity.java
+++ app/src/main/java/programs/taskoid/TaskListActivity.java
@@ -2,6 +2,8 @@ package programs.taskoid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TaskListActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TaskListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_list);
+
+        String[] items = {"1","2","3"};
+
+        ListView list = (ListView)findViewById(R.id.task_list);
+        list.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items));
+
+
+
     }
 }
