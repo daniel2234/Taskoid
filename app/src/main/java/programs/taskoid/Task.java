0c7cb6f9cd39a92cf3367339eeb7bb71611a0669
Daniel Kwiatkowski <kwiatkowski.daniel@gmail.com>
2016-03-29 22:34:53 -0400

    Initial commit and added a Task class

---
+++ app/src/main/java/programs/taskoid/Task.java
@@ -0,0 +1,12 @@
+package programs.taskoid;
+
+import java.util.Date;
+
+/**
+ * Created by Daniel on 2016-03-29.
+ */
+public class Task {
+    private String mName;
+    private Date mDueDate;
+    private boolean mDone;
+}
